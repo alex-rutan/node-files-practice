589880b6d3b897afaeb84a9ceb56361aeaca4391
Alex  Rutan <alexrutan04@gmail.com>
2021-07-27 12:54:40 -0700

    finished part2 catOrWebCat

--- step2.js
+++ step2.js
@@ -1,44 +1,58 @@
-const cat = require("./step1");
+"use strict"
+
+const fsP = require('fs/promises')
 const axios = require("axios");
 
-/* 
-takes in a URL path, outputs the contents to the console.
- */
-async function webCat(url) {
-    if(!url.startsWith("http")){
-        url = "http://"+url
-    }
+
+async function cat(path) {
     try {
-        let content = await axios.get(url);
-        console.log(content.data);
+        let contents = await fsP.readFile(path, "utf8");
+        console.log(contents);
     } catch (err) {
         console.log(err);
         process.exit(1);
     }
 }
 
+
+/* 
+takes in a URL path, outputs the contents to the console.
+ */
+async function webCat(url) {
+    console.log("WebCat is Running!!!")
+    // if(!url.startsWith("http")){
+    //     url = "http://"+url
+    // }
+    let content;
+    try {
+        content = await axios.get(url);
+    } catch (err) {
+        console.log(err);
+        process.exit(1);
+    }
+    console.log(content.data);
+}
+
 /* 
 Function to determine if the argument given is a local path or web path
 then runs cat function or webCat respectively.
  */
-async function catOrWebCat(){
+async function catOrWebCat() {
     
     let path = process.argv[2];
+    console.log(path);
     //regex to determine if the argument is website or relative path
     let regex = /^(www|http|https)*.(\w+)/g;
     let matches = path.match(regex);
-    let isWebsite = matches.length >= 1;
-    
+    let isWebsite = matches instanceof Array;
     
     if (isWebsite){
         console.log("running webCat")
-        await webCat(path);
+        webCat(path);
     }else{
         console.log("running Cat")
         cat(path);        
-    }
-
-    
+    }   
 }
 
 catOrWebCat()
