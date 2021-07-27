fcc1d12fe801a76f527716fea763a3dc1230fa05
magus0 <harvestashya@gmail.com>
2021-07-27 12:21:50 -0700

    differentiating cat from webcat

--- step2.js
+++ step2.js
@@ -1,13 +1,44 @@
 const cat = require("./step1");
 const axios = require("axios");
 
+/* 
+takes in a URL path, outputs the contents to the console.
+ */
 async function webCat(url) {
-    let content = await axios.get(url);
-    console.log(content);
+    if(!url.startsWith("http")){
+        url = "http://"+url
+    }
+    try {
+        let content = await axios.get(url);
+        console.log(content.data);
+    } catch (err) {
+        console.log(err);
+        process.exit(1);
+    }
 }
 
-async function catOrWebCat (path)
-// let path = process.argv[2];
-    if (path )
+/* 
+Function to determine if the argument given is a local path or web path
+then runs cat function or webCat respectively.
+ */
+async function catOrWebCat(){
+    
+    let path = process.argv[2];
+    //regex to determine if the argument is website or relative path
+    let regex = /^(www|http|https)*.(\w+)/g;
+    let matches = path.match(regex);
+    let isWebsite = matches.length >= 1;
+    
+    
+    if (isWebsite){
+        console.log("running webCat")
+        await webCat(path);
+    }else{
+        console.log("running Cat")
+        cat(path);        
+    }
 
-    cat(path);
+    
+}
+
+catOrWebCat()
